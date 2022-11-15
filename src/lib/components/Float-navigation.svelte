5cc3c2609f547b1211b278bfcd498e114478be81
Emona Santiago <emonasantiago1207@gmail.com>
2022-11-15 13:43:54 +0100

    .

---
+++ src/lib/components/Float-navigation.svelte
@@ -0,0 +1,82 @@
+<script>
+  export let NavigationLink;
+  export let standardHeaderTitle;
+  let isActive = false;
+
+  function toggleMenu() {
+    isActive = !isActive;
+  }
+</script>
+
+<div>
+  <strong on:click={toggleMenu}>
+    <strong>Inhoud {standardHeaderTitle}</strong>
+    {#if isActive}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="icon icon-tabler icon-tabler-square-x"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <rect x="4" y="4" width="16" height="16" rx="0" />
+        <path d="M10 10l4 4m0 -4l-4 4" />
+      </svg>
+    {/if}
+  </strong>
+  <nav class:active={!isActive}>
+    <ul>
+      <slot />
+    </ul>
+  </nav>
+</div>
+
+<style>
+  div {
+    background-color: yellow;
+    position: fixed;
+    bottom: 1rem;
+    right: 1rem;
+    text-align: left;
+    padding: 0.5rem;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.15);
+    display: flex;
+    flex-direction: column;
+    max-width: 20em;
+    z-index: 10;
+  }
+
+  strong {
+    font-size: 0.8rem;
+    cursor: pointer;
+    padding: 0 0.15rem;
+  }
+
+  svg {
+    position: absolute;
+    right: -0.25rem;
+    top: -2rem;
+    stroke: black;
+    fill: yellow;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  nav.active {
+    display: none;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    margin-top: 0.25rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    import FloatNavigation from "../lib/components/Float-navigation.svelte";
+</script>
+<!-- 
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<FloatNavigation></FloatNavigation>
