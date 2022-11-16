57baad3e79ec0cb7a383708001c6b7a07766180d
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-11-16 15:35:56 +0100

    Finished the footer component #7

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,27 @@
+<footer>
+	<span>© 2022 CMD AMSTERDAM</span>
+</footer>
+
+<style>
+	footer {
+		border: 3px solid black;
+		text-align: right;
+		padding: 1rem;
+	}
+
+	span {
+		font-weight: 600;
+	}
+
+	@media (min-width: 60rem) {
+		footer {
+			padding: 1rem 2rem;
+		}
+	}
+
+	@media (min-width: 81.25rem) {
+		footer {
+			padding: 1rem 5rem;
+		}
+	}
+</style>
