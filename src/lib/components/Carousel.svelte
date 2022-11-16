305a50becff5c082e4aee60fe0e3b8e5c07da664
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2022-11-16 13:10:06 +0100

    base setup carousel

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,91 @@
+<script>
+</script>
+
+   <main>    
+    <section class="carousel">
+       <section class="carousel-card-body">
+
+         <div class="card">
+            <h1> Organisaties als opdrachtgever voor projecten, stages en afstudeerprojecten</h1>
+            <p>Vanaf het tweede jaar wordt bij projecten vaak samengewerkt met externe projectpartners. In de meeste gevallen komen projectpartners uit het netwerk van de bij het vak betrokken docenten, maar het komt ook voor dat ze worden aangedragen door lectoraten, kenniscentra, bedrijven, en de coördinator externe samenwerking.
+               Studenten komen naast projectopdrachten ook door twee stageperiodes in aanraking met het werkveld. Er is een korte oriënterende stage van 10 weken aan het eind van het fundament waarin de studenten de in het fundament verworven competenties in de praktijk toetsen. Studenten werken in principe aan alle competenties, 
+               maar de nadruk ligt op vier competenties. Eén competentie, multidisciplinair samenwerken, is verplicht, de andere drie kiest de student zelf. Voor de vier competenties formuleren studenten zelf de leerdoelen. Tijdens de lange stage werken studenten aan de beroepsidentiteit en het (door)ontwikkelen van CMD-competenties. Bij de lange stage zijn de competenties multidisciplinair samenwerken en één van de vakgebied competenties verplicht. In totaal werken studenten aan tenminste vijf competenties.
+            </p>
+         </div>
+         
+         <div class="card">
+            <h1>CMD Agency</h1>
+            <p>Hoewel de korte stage een belangrijke functie heeft in de beroepsvorming van studenten, geven bedrijven aan dat de periode erg kort is om zinvol te zijn. Daarnaast merkten we dat het voor sommige studenten minder toegevoegde waarde heeft. Bijvoorbeeld omdat ze al kennis hebben gemaakt met het bedrijfsleven tijdens de vooropleiding of omdat ze de ambitie hebben om als zelfstandige aan de slag te gaan. Voor deze studenten is CMD Agency opgericht. Studenten kunnen kiezen tussen de korte stage of in teams werken aan echte opdrachten van echte opdrachtgevers. Er is een sollicitatieprocedure waarbij gekeken wordt naar het vermogen om zelfstandig te werken, communicatieve vaardigheden en uiteraard inhoudelijke kennis en skills.
+               De teams zijn grotendeels zelfsturend en zelf verantwoordelijk voor planning en communicatie met de opdrachtgever. Ter ondersteuning van het project, krijgen de studenten een inleiding in SCRUM, daarna overleggen ze met de opdrachtgever die als Product Owner fungeert. Teams krijgen een teamcoach van CMD toegewezen én een individuele docentbegeleider. Verder zijn studentassistenten beschikbaar
+            </p>
+         </div>
+
+         <div class="card">
+            <h1>Gastcolleges</h1>
+            <p>Naast projecten en stages worden regelmatig experts uit het vakgebied uitgenodigd om een gastcollege te verzorgen. Ook organiseren docenten ICONS: een lezingenreeks door kopstukken uit de industrie voor studenten, docenten en vakgenoten.
+            </p>
+         </div>
+
+         <div class="card">
+            <h1>Opleidingsadviesraad</h1>
+            <p>Een belangrijke schakel met het beroepenveld is de Opleidingsadviesraad (OAR). De OAR heeft een adviserende rol op strategisch (bijv. ontwikkelingen in het vakgebieden daarbuiten) en inhoudelijk gebied (bijv. eindkwalificaties van CMD studenten, invulling curriculum). Na de vorige accreditatie is, samen met docenten en de huidige leden, gekeken naar de opzet van de OAR, wat leidde tot een nieuwe vorm en aangepaste samenstelling. Voor wat betreft de samenstelling zijn strategische keuzes gemaakt. Van een aantal leden is afscheid genomen, daarvoor in de plaats zijn organisaties gekomen die meer aansluiten en die bij voorkeur affiniteit hebben met de kernwaarden van de HvA. De huidige samenstelling van de OAR is hier te bekijken. Er is nog ruimte voor 1 of 2, bij voorkeur non-profit, organisaties.
+            </p>
+         </div>
+
+         <div class="card">
+            <h1>Partners in Education</h1>
+            <p>De wens van een aantal bedrijven om een meer structurele en langdurige samenwerking met CMD Amsterdam te hebben heeft geleid tot een nieuw initiatief: Partners in Education (PiE). De missie van PiE is om de samenwerking met (internationale) externe partijen en interne stakeholders op een professionele, efficiënte en duurzame wijze vorm te geven. We willen externe partijen (o.a. werkveld en onderwijspartners) laten zien dat wij (duurzame) samenwerking hoog op de agenda hebben staan. Door vanuit PiE organisatorische en administratieve ondersteuning te bieden aan interne stakeholders (o.a. docenten en medewerkers) krijgen zij meer tijd om inhoudelijk hun onderwijs vorm te geven.
+            </p>
+         </div>
+
+       </section>
+
+    </section>
+</main>
+<style>
+   
+    .carousel-card-body{
+      display: grid;
+      scroll-snap-type: x mandatory;
+      overflow-x: scroll;
+
+      grid: auto / auto-flow max-content;
+      grid-gap: 5em;
+      align-items: center;
+      padding: 2.5em;
+      scroll-snap-type:mandatory ;
+    }
+
+    .card{
+      scroll-snap-align: start;
+      width: 25em;
+      height: 30em;
+      padding: 2.5em;
+      border: 2px solid black;
+      box-shadow: -7px 7px 0px 0px #000;
+      background-color: yellow;
+    }
+/* width */
+::-webkit-scrollbar {
+  width: 10px; 
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  border: 1px solid #000; 
+
+
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #000; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(255, 251, 0); 
+}
+   
+ 
+</style>
