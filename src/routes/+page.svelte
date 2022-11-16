d7d7a026dc203c06e9060c646825ab92b1e9ffe0
DaanKorver <daank2000@hotmail.nl>
2022-11-16 12:52:54 +0100

    Added TitleTextAsset component

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import TitleTextAsset from '../lib/components/TitleTextAsset.svelte';
+</script>
+
+<TitleTextAsset
+	title="Een titel ofzo"
+	text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illum ad distinctio in dignissimos at esse alias repellendus debitis asperiores ipsa sint, possimus eos officia beatae quidem quod illo quasi unde laudantium cumque dolorum nam quas a! Eaque autem eius provident veritatis tempora quaerat tenetur possimus, consequuntur, asperiores illum error.  "
+	src="https://placebear.com/500/300"
+	altText="bear"
+/>
