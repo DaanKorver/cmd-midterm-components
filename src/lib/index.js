15d13b05e97e59963ff2b298da8f90d09b8acc07
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-11-16 15:41:25 +0100

    Added import and export in the index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,7 @@
 // Reexport your entry components here
 import Cursor from '$lib/components/cursor.svelte';
+import Footer from '$lib/components/footer.svelte';
+
 
 export { Cursor };
+export { Footer };
