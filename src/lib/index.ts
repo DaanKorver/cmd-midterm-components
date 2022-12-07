4001d4ed4eb2b53bf53a169be5abb158a721c628
DaanKorver <daank2000@hotmail.nl>
2022-12-07 10:12:41 +0100

    Update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,11 @@
 // Reexport your entry components here
 
-export { default as Header } from './components/Header.svelte';
-export { default as Link } from './components/Link.svelte';
-export { default as Footer } from './components/Footer.svelte';
-export { default as FullCarousel } from './components/FullCarousel/FullCarousel.svelte';
-export { default as MainHero } from './components/Mainhero.svelte';
-export { default as TitleTextAsset } from './components/Mainhero.svelte';
-export { default as Cursor } from './components/Cursor.svelte';
 export { default as Carousel } from './components/Carousel.svelte';
 export { default as ChapterContent } from './components/ChapterContent.svelte';
+export { default as Cursor } from './components/Cursor.svelte';
+export { default as Footer } from './components/Footer.svelte';
+export { default as FullCarousel } from './components/FullCarousel/FullCarousel.svelte';
+export { default as Header } from './components/Header.svelte';
+export { default as Link } from './components/Link.svelte';
+export { default as MainHero, default as TitleTextAsset } from './components/Mainhero.svelte';
+export { default as StandardSectionButton } from './components/StandardSectionButton.svelte';
